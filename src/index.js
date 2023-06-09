bf2a1346d23ccae393819efd8025b7448e37f16e
Joseph Ruiz <Ruiz-joseph@hotmail.com>
2023-06-09 11:30:00 -0400

    some fundamental fixes

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import start from '../src/pages/start.js'
+import Start from '../src/pages/start.js'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <b>Hello World!</b>
-    <start/>
+    <Start/>
   </React.StrictMode>
 );
